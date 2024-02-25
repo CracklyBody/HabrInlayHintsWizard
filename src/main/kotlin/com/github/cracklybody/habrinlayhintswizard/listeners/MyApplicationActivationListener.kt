a45c28c66889e335e3ef5b3b5c0fd2402b282f05
GitHub Action <action@github.com>
2024-02-25 19:55:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/cracklybody/habrinlayhintswizard/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/cracklybody/habrinlayhintswizard/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cracklybody.habrinlayhintswizard.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
